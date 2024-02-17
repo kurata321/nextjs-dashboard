ed1ce65f0a95a005ac0ea677d3ff0927ae450dba
kurata321 <56961325+kurata321@users.noreply.github.com>
2024-02-17 15:51:42 +0900

    up to chapter 17

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,9 +4,13 @@ import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
-import { CardSkeleton, InvoicesTableSkeleton } from '@/app/ui/skeletons'
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons'
 import { Suspense } from 'react';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Invoices'
+};
 export default async function Page({
     searchParams,
   }: {
